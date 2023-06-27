0829bd3b40f9ca04677d82a50bc04d7faba11b29
Tina Corluka <krigla@gmail.com>
2023-06-28 00:31:14 +0200

    More stuff font new team page

---
+++ pages/GlobalStyles.js
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  :root {
+    --dark-primary: rgba(14, 1, 50, 1);
+    --light-primary: rgb(255, 253, 237);
+    --light-secondary: bisque;
+
+    --text-primary: var(--light-primary);
+    --text-invert: var(--dark-primary);
+  }
+
+  body {
+    min-height: 100vh;
+    color: var(--text-primary);
+    background: linear-gradient(
+      156deg,
+      rgba(14, 1, 50, 1) 0%,
+      rgba(39, 41, 109, 1) 49%,
+      rgba(103, 12, 212, 1) 91%,
+      rgba(130, 0, 255, 1) 100%
+    );
+  }
+
+  button,
+  [type="button"],
+  [type="reset"],
+  [type="submit"] {
+    padding: 15px 25px;
+    background-color: var(--light-secondary);
+    color: var(--text-invert);
+    border-radius: 100px;
+
+    &[disabled] {
+      opacity: 0.7;
+    }
+  }
+`;
+
+export default GlobalStyles;
--- pages/team/new/index.js
+++ pages/team/new/index.js
@@ -1,30 +1,47 @@
 import styled from "styled-components";
+import { useState } from "react";
 import { mutations, withApiDataMutation } from "../../../api";
 
 function CreateNewTeam(props) {
+  const [name, setName] = useState("");
+  const [logoSrc, setLogoSrc] = useState("");
   const { createTeam } = props;
 
   const onSubmit = (e) => {
     e.preventDefault();
-    // just for testing
     createTeam({
       variables: {
         data: {
-          name: "123456789",
-          logoSrc:
-            "https://images.dog.ceo/breeds/mastiff-bull/n02108422_1548.jpg",
+          name,
+          logoSrc,
         },
       },
     });
+  };
+  const handleNameChange = (e) => {
+    e.preventDefault();
+    setName(e.target.value);
+  };
+
+  const handleLogoChange = (e) => {
+    e.preventDefault();
+    setLogoSrc(e.target.value);
   };
 
   return (
     <Box>
       <Title>Create new team</Title>
-      <form onSubmit={onSubmit}>
-        <input />
-        <button type="submit">Create new team</button>
-      </form>
+      <Form onSubmit={onSubmit}>
+        <InputBox>
+          <Input value={name} onChange={handleNameChange} type="text" />
+        </InputBox>
+        <InputBox>
+          <Input value={logoSrc} onChange={handleLogoChange} type="url" />
+        </InputBox>
+        <button type="submit" disabled={!(name && logoSrc)}>
+          Create new team
+        </button>
+      </Form>
     </Box>
   );
 }
@@ -42,6 +59,23 @@ const Title = styled.h1`
   font-weight: 700;
   font-size: 2.25rem;
   line-height: 2.5rem;
+`;
+
+const InputBox = styled.div`
+  margin-bottom: 20px;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  background-color: transparent;
+  border: 2px solid var(--light-secondary);
+  height: 48px;
+  border-radius: 300px;
+  padding: 0 20px;
 `;
 
 export default withApiDataMutation(
