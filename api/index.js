948af23f10cc3f5f00eb8fd29de1fb4fa9bc8a59
Tina Corluka <krigla@gmail.com>
2023-06-29 14:53:29 +0200

    Remove unused helper

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
 import * as mutations from "./mutations";
 import * as queries from "./queries";
 export * from "./withApiData";
-export * from "./withApiDataMutation";
 
 export { mutations, queries };
