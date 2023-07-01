2e0903c424159836dd64a267242fb98d21955293
JohannesMerz <johannes@bojagi.io>
2023-07-01 16:14:18 +0200

    provider and message consumer link

--- components/Feedback.js
+++ components/Feedback.js
@@ -1,56 +1,113 @@
 // concept
 
-const { useMemo, createContext, useContext, useCallback } = require("react")
+const {
+  useMemo,
+  createContext,
+  useContext,
+  useCallback,
+  useEffect,
+  useRef,
+} = require("react");
 
-function myComponent() {
-  const feedback = useFeedback()
+// function myComponent() {
+//   const feedback = useFeedback();
 
-  feedback.open({
-    message: 'hans',
-    mode: 'ERROR',
-    timeout: 2000, // when set to null has to be closed manually
-    onClose: (closedByUser) => console.log('closed ' + closedByUser ? 'by user' : 'after timeout') // optional
-  })
+//   feedback.open({
+//     message: "hans",
+//     mode: "ERROR",
+//     timeout: 2000, // when set to null has to be closed manually
+//     onClose: (closedByUser) =>
+//       console.log("closed " + closedByUser ? "by user" : "after timeout"), // optional
+//   });
 
-  // simple neutral info message visible for 3 s
-  feedback.open({
-    message: 'huhu',
-  })
+//   // simple neutral info message visible for 3 s
+//   feedback.open({
+//     message: "huhu",
+//   });
 
-  feedback.open({
-    message: 'amazing!',
-    mode: 'SUCCESS',
-    timeout: null,
-    onClose: () => {
-      console.log('continue with user flow')
-    }
-  })
+//   feedback.open({
+//     message: "amazing!",
+//     mode: "SUCCESS",
+//     timeout: null,
+//     onClose: () => {
+//       console.log("continue with user flow");
+//     },
+//   });
 
-  feedback.isOpen() // true/false
-  feedback.close() // force close programmatically
+//   feedback.isOpen(); // true/false
+//   feedback.close(); // force close programmatically
+// }
 
+const FeedbackContext = createContext();
+
+// the provider is just a link from the consumer to the context, we don't have any state in here to prevent big tree
+// updates as the provider is at the base of the app
+function FeedbackProvider({ children }) {
+  const consumer = useRef(null);
+
+  const messageConsumer = useCallback((method) => {
+    return (...args) => consumer.current[method](...args);
+  }, []);
+
+  const value = useMemo(
+    () => ({
+      registerConsumer: (instance) => (consumer.current = instance),
+      open: messageConsumer("open"),
+      close: messageConsumer("close"),
+      isOpen: messageConsumer("isOpen"),
+    }),
+    []
+  );
+
+  return (
+    <FeedbackContext.Provider value={value}>
+      {children}
+    </FeedbackContext.Provider>
+  );
 }
 
-const feedbackContext = createContext()
+function FeedbackDisplay() {
+  const context = useContext(FeedbackContext);
 
-const defaultCb = () => {}
+  console.log(context);
+
+  const open = useCallback(() => {
+    console.log("opening feedback consumer");
+  }, []);
+
+  const close = useCallback(() => {
+    console.log("closing feedback consumer");
+  }, []);
+
+  useEffect(() => {
+    context.registerConsumer({
+      open,
+      close,
+    });
+  }, []);
+
+  return null;
+}
+
+// const open = useCallback(
+//   ({ message, mode = "INFO", timeout = 3000, onClose = defaultCb }) => {
+//     console.log({ message, mode, timeout, onClose });
+//     f.open;
+//   },
+//   []
+// );
+
+// return useMemo(
+//   () => ({
+//     open,
+//   }),
+//   [open]
+// );
+
+// const defaultCb = () => {};
 
 function useFeedback() {
-
-  const feedbackContext = useContext(feedbackContext)
-
-  const open = useCallback(({
-    message,
-    mode = 'INFO',
-    timeout = 3000,
-    onClose = defaultCb
-  }) =>{
-    console.log({message, mode, timeout, onClose})
-
-  }, [])
-
-
-  return useMemo(() => ({
-    open
-  }),[open])
+  return useContext(FeedbackContext);
 }
+
+export { FeedbackDisplay, FeedbackProvider, FeedbackContext, useFeedback };
--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,18 @@ import client from "../api/apolloClient";
 import Layout from "../components/Layout";
 import "../globals.css";
 import GlobalStyles from "./GlobalStyles";
+import { FeedbackDisplay, FeedbackProvider } from "@/components/Feedback";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <ApolloProvider client={client}>
-      <GlobalStyles />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <FeedbackProvider>
+        <GlobalStyles />
+        <FeedbackDisplay />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </FeedbackProvider>
     </ApolloProvider>
   );
 }
